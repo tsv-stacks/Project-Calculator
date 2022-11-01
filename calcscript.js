e073d2a3af028fab2101f5344d7f072e59d9295f
tsv-stacks <tsvirdi@yahoo.co.uk>
2022-11-01 20:41:19 +0000

    added span for js dom manip

---
+++ calcscript.js
@@ -0,0 +1,7 @@
+// push number to array
+// operator selector button press adds code or function
+// push number to array
+// equals sign to calculate
+// operate function runs on = (takes 2 numbers & operator and returns)
+
+// pressing number clears calculator
