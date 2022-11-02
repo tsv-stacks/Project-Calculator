eb56b7b6f8806e3815942825651bace03af3e14f
tsv-stacks <tsvirdi@yahoo.co.uk>
2022-11-02 00:11:19 +0000

    added operators to js

--- calcscript.js
+++ calcscript.js
@@ -5,3 +5,41 @@
 // operate function runs on = (takes 2 numbers & operator and returns)
 
 // pressing number clears calculator
+
+let operator = ""
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return "ERROR!"
+    } else {
+        return num1 / num2
+    }
+}
+
+function percentage(num) {
+    return num / 100
+}
+
+function operate(num1, num2, operator) {
+    if (operator == "add") {
+        return add(num1, num2)
+    } else if (operator == "subtract") {
+        return subtract(num1, num2)
+    } else if (operator == "multiply") {
+        return multiply(num1, num2)
+    } else if (operator == "divide") {
+        return divide(num1, num2)
+    }
+}
