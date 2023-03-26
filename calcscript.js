192aad68b10c00ff54a7e81469c2506364eec304
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-03-27 00:05:31 +0100

    cleaned up code and updated readme

--- calcscript.js
+++ calcscript.js
@@ -1,226 +1,188 @@
-let displayArray = []
+let displayArray = [];
 let solutionOutput = document.getElementById("solution");
 let displayValue = document.getElementById("display-value");
-let operator = ""
-let numbersinArray = []
-let operatorType = []
-let splitNumArray = []
-let result = 0
+let operator = "";
+let numbersinArray = [];
+let operatorType = [];
+let splitNumArray = [];
+let result = 0;
 
-displayValue.textContent = displayArray.join('')
-
-// add keyboard support
-
-// code % button
-
-// fix number and pressing equal !== NaN
-
+displayValue.textContent = displayArray.join("");
 // function to split array into two numbers
 function arrayToNum() {
-    console.log(displayArray)
-    let operatorPosition = []
+  let operatorPosition = [];
 
-    for (let i = 0; i < displayArray.length; i++) {
-        if (idCheck(displayArray[i]) == false) {
-            operatorPosition.push(i)
-        }
+  for (let i = 0; i < displayArray.length; i++) {
+    if (idCheck(displayArray[i]) == false) {
+      operatorPosition.push(i);
     }
+  }
 
-    for (let i = 0; i < displayArray.length; i++) {
-        if (displayArray[i] == ' + ') {
-            operatorType.push("add")
-        } else if (displayArray[i] == ' − ') {
-            operatorType.push("subtract")
-        } else if (displayArray[i] == ' ÷ ') {
-            operatorType.push("divide")
-        } else if (displayArray[i] == " × ") {
-            operatorType.push("multiply")
-        }
+  for (let i = 0; i < displayArray.length; i++) {
+    if (displayArray[i] == " + ") {
+      operatorType.push("add");
+    } else if (displayArray[i] == " − ") {
+      operatorType.push("subtract");
+    } else if (displayArray[i] == " ÷ ") {
+      operatorType.push("divide");
+    } else if (displayArray[i] == " × ") {
+      operatorType.push("multiply");
     }
-    // console.log(numberArrayGen())
-
-    console.log(operatorType)
-
-    let numbersinArray = numberArrayGen()
-    splitNumArray = numbersinArray.join("")
-    splitNumArray = splitNumArray.split(' ').map(Number)
-
-    console.log(splitNumArray)
-    console.log(splitNumArray.length)
-    console.log(operatorType)
-    console.log(operatorType.length)
-
-
-    calculation()
-
-
-
-
-    console.log(numbersinArray)
-
-    backendClear()
+  }
+  let numbersinArray = numberArrayGen();
+  splitNumArray = numbersinArray.join("");
+  splitNumArray = splitNumArray.split(" ").map(Number);
+  calculation();
+  backendClear();
 }
 
 function backendClear() {
-    result = 0
-    operator = ""
-    numbersinArray = []
-    operatorType = []
-    splitNumArray = []
+  result = 0;
+  operator = "";
+  numbersinArray = [];
+  operatorType = [];
+  splitNumArray = [];
 }
 
 function calculation() {
-    if (splitNumArray.length == 1 && operatorType.length == 0) {
-        console.log("single number")
-        return solutionOutput.textContent = splitNumArray
-    } else if (splitNumArray.length === 1 && operatorType.length >= 1) {
-        console.log("running 1")
-        num1 = result
-        num2 = splitNumArray[0]
-        operator = operatorType[0]
-        let finalresult = operate(num1, num2, operator)
-        console.log(operatorType)
-        console.log(result)
-        return solutionOutput.textContent = Math.round((finalresult + Number.EPSILON) * 1000000) / 1000000
-    } else if (splitNumArray.length == 2 && operatorType.length == 1) {
-        console.log("running 2")
-        num1 = splitNumArray[0]
-        num2 = splitNumArray[1]
-        operator = operatorType[0]
-        result += operate(num1, num2, operator)
-        console.log(result)
-        return solutionOutput.textContent = Math.round((result + Number.EPSILON) * 1000000) / 1000000
-    } else if (splitNumArray.length === operatorType.length) {
-        console.log("extra operator at end")
-        operatorType.pop()
-        calculation()
-    } else {
-        console.log("running 3")
-        num1 = splitNumArray[0]
-        num2 = splitNumArray[1]
-        operator = operatorType[0]
-        splitNumArray.shift()
-        splitNumArray.shift()
-        operatorType.shift()
-        result += operate(num1, num2, operator)
-        console.log(result)
-        calculation()
-    }
+  if (splitNumArray.length == 1 && operatorType.length == 0) {
+    return (solutionOutput.textContent = splitNumArray);
+  } else if (splitNumArray.length === 1 && operatorType.length >= 1) {
+    num1 = result;
+    num2 = splitNumArray[0];
+    operator = operatorType[0];
+    let finalresult = operate(num1, num2, operator);
+    return (solutionOutput.textContent =
+      Math.round((finalresult + Number.EPSILON) * 1000000) / 1000000);
+  } else if (splitNumArray.length == 2 && operatorType.length == 1) {
+    num1 = splitNumArray[0];
+    num2 = splitNumArray[1];
+    operator = operatorType[0];
+    result += operate(num1, num2, operator);
+    return (solutionOutput.textContent =
+      Math.round((result + Number.EPSILON) * 1000000) / 1000000);
+  } else if (splitNumArray.length === operatorType.length) {
+    operatorType.pop();
+    calculation();
+  } else {
+    num1 = splitNumArray[0];
+    num2 = splitNumArray[1];
+    operator = operatorType[0];
+    splitNumArray.shift();
+    splitNumArray.shift();
+    operatorType.shift();
+    result += operate(num1, num2, operator);
+    calculation();
+  }
 }
 
 function numberArrayGen() {
-    for (let i = 0; i < displayArray.length; i++) {
-        if (idCheck(displayArray[i]) == false) {
-            numbersinArray.push(" ")
-        } else {
-            numbersinArray.push(displayArray[i])
-        }
+  for (let i = 0; i < displayArray.length; i++) {
+    if (idCheck(displayArray[i]) == false) {
+      numbersinArray.push(" ");
+    } else {
+      numbersinArray.push(displayArray[i]);
     }
-    return numbersinArray
+  }
+  return numbersinArray;
 }
 
-// Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+let inputButton = document.getElementsByClassName("input");
 
-let inputButton = document.getElementsByClassName("input")
+const buttonInputs = document.querySelectorAll(".input");
 
-const buttonInputs = document.querySelectorAll(".input")
-
-buttonInputs.forEach(buttonInput => {
-    buttonInput.addEventListener('click', handleClick)
+buttonInputs.forEach((buttonInput) => {
+  buttonInput.addEventListener("click", handleClick);
 });
 
 function handleClick(e) {
-    let { id } = e.target
-    let arrayEnd = displayArray[displayArray.length - 1]
+  let { id } = e.target;
+  let arrayEnd = displayArray[displayArray.length - 1];
 
-    if (displayArray.join('').length > 36) {
-        // arr max length = 24
-        return console.log("max length")
-    } else if (displayArray.length == 0 && idCheck(id) == false) {
-        // prevent operator at start
-        return operator = ""
-    } else if (idCheck(id) == false && idCheck(arrayEnd) == false) {
-        return console.log("multiple operator error")
-        // prevent operator after operator
-    } else {
-        displayArray.push(id);
-        displayValue.textContent = displayArray.join('')
-    }
-    // console.log("array length = " + displayArray.length)
-    lengthCheck(displayArray)
+  if (displayArray.join("").length > 36) {
+    // arr max length = 24
+    return console.log("max length");
+  } else if (displayArray.length == 0 && idCheck(id) == false) {
+    // prevent operator at start
+    return (operator = "");
+  } else if (idCheck(id) == false && idCheck(arrayEnd) == false) {
+    return console.log("multiple operator error");
+    // prevent operator after operator
+  } else {
+    displayArray.push(id);
+    displayValue.textContent = displayArray.join("");
+  }
+  lengthCheck(displayArray);
 }
 
 // changes display array font dependent on length
 function lengthCheck(arr) {
-    let a = arr.join('').length
-    if (a < 18) {
-        return displayValue.style["font-size"] = "xx-large"
-    } else if (a > 27) {
-        return displayValue.style["font-size"] = "large"
-    } else if (a > 18) {
-        return displayValue.style["font-size"] = "x-large"
-    }
+  let a = arr.join("").length;
+  if (a < 18) {
+    return (displayValue.style["font-size"] = "xx-large");
+  } else if (a > 27) {
+    return (displayValue.style["font-size"] = "large");
+  } else if (a > 18) {
+    return (displayValue.style["font-size"] = "x-large");
+  }
 }
 
 function idCheck(id) {
-    if (id == undefined) {
-        return false
-    } else {
-        return id.match(/^[0-9]+$/) != null
-    }
+  if (id == undefined) {
+    return false;
+  } else {
+    return id.match(/^[0-9]+$/) != null;
+  }
 }
 
 //backspace clear , if statement reset operator
 
 function backspaceCLR() {
-    let popped = displayArray.pop()
-    console.log(popped)
-    displayValue.textContent = displayArray.join('')
+  let popped = displayArray.pop();
+  displayValue.textContent = displayArray.join("");
 }
 
 // pressing number clears calculator
 function clearbtn() {
-    solutionOutput.textContent = ""
-    displayValue.textContent = ""
-    operator = ""
-    displayArray = []
+  solutionOutput.textContent = "";
+  displayValue.textContent = "";
+  operator = "";
+  displayArray = [];
 }
 
 // maths functions
 function add(num1, num2) {
-    return num1 + num2
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-    if (num2 === 0) {
-        return solutionOutput.textContent = "ERROR!"
-    } else {
-        return num1 / num2
-    }
+  if (num2 === 0) {
+    return (solutionOutput.textContent = "ERROR!");
+  } else {
+    return num1 / num2;
+  }
 }
 
 function percentage(num) {
-    return num / 100
+  return num / 100;
 }
 
-//Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-
 function operate(num1, num2, operator) {
-    if (operator == "add") {
-        return solutionOutput.textContent = add(num1, num2)
-    } else if (operator == "subtract") {
-        return solutionOutput.textContent = subtract(num1, num2)
-    } else if (operator == "multiply") {
-        return solutionOutput.textContent = multiply(num1, num2)
-    } else if (operator == "divide") {
-        return solutionOutput.textContent = divide(num1, num2)
-    }
+  if (operator == "add") {
+    return (solutionOutput.textContent = add(num1, num2));
+  } else if (operator == "subtract") {
+    return (solutionOutput.textContent = subtract(num1, num2));
+  } else if (operator == "multiply") {
+    return (solutionOutput.textContent = multiply(num1, num2));
+  } else if (operator == "divide") {
+    return (solutionOutput.textContent = divide(num1, num2));
+  }
 }
--- readme.md
+++ readme.md
@@ -1,18 +1,21 @@
-html
+# Calculator
 
-- header
-- main div
-- footer
+### Live Page: https://tsv-stacks.github.io/Project-Calculator/
 
-css
+A simple calculator app built with HTML, CSS, and vanilla JavaScript. This app allows users to perform basic arithmetic operations (addition, subtraction, multiplication, and division) on two numbers. This was one of the first apps I built and helped me learn more about arrays and array manipulation.
 
-- flexbox
+## Installation
 
-add stylings and bg
+To use the calculator, simply clone this repository to your local machine and open `index.html` in your browser.
 
-- color on hover
-- button press
+```bash
+ git clone https://github.com/tsv-stacks/Project-Calculator
+ cd Project-Calculator
+ open index.html
+```
 
-js
+## Future updates
 
-- output
+- add keyboard support
+- code % button
+- fix number and pressing equal !== NaN
